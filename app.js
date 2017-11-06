862d6abf098043d4bad0c012d12dd58aa74fecc5
Jhow <jhowliu@gmail.com>
2017-11-06 18:27:56 +0800

    finished to combine line and bot control-flow

--- app.js
+++ app.js
@@ -3,29 +3,44 @@ const bodyParser = require('body-parser');
 const line = require('messaging-api-line');
 
 const meta = require('./config/line-meta');
+const Parser = require('./lib/parse');
+const dialogControl = require('./lib/dialogue');
+const utils = require('./lib/utils'); 
+
+const client = line.LineClient.connect(meta.accessToken, meta.secret);
 
 const app = express();
 
-const client = line.LineClient.connect(meta.accessToken, meta.secret);
+const redis = require('./lib/redis');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json('application/json'));
 
 const port = process.env.PORT || 8080;
 
-client.pushText(meta.userid, 'PUSH TEXT')
-  .then(() => {
+client.pushText(meta.userid, 'PUSH TEXT').then(() => {
     console.log('PUSH TEXT');
-  })
-  .catch((err) => {
+}).catch((err) => {
     console.log(err);
 });
 
 app.post('/webhook', (req, res) => {
-  console.log(req.body); 
-  return res.send({ success: true, msg: 'please wait response' });
+    // Parse events to users (Promise Object)
+    users = Parser.parse(req.body.events);
+
+    const promises = users.map(user => {
+        return dialogControl.dialog(user);
+    });
+
+    Promise.all(promises).then(replies => {
+        replies.map(reply => {
+            client.replyText(reply.token, reply.text);
+        });
+    });
+
+    return res.send({ success: true, msg: 'please wait response' });
 });
 
 app.listen(port, () => {
-  console.log('Server start on PORT: '+ port);
+    console.log('Server start on PORT: '+ port);
 });
